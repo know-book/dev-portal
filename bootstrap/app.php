a983656c6c23e1fdee26d56555cfcc426480a8e2
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2026-07-25 13:31:22 +0800

    fix: exclude api/webhooks/github from CSRF protection in bootstrap/app.php to resolve 419 error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             SetTeamUrlDefaults::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/webhooks/github',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
